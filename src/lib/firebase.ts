5e15d8af4e5792fc04496639eb7196254392f344
Chan Chun Hin <chan@owndays.co.jp>
2021-10-26 16:15:56 +0900

    init

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,8 +3,8 @@ import "firebase/auth"; // If you need it
 
 export const config = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMEIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  //: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, // realtime database
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
